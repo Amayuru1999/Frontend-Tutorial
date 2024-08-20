30c913d7b1d8772310820b9c6041a8f7da84fe42
Amayuru1999 <amarasingheau@gmail.com>
2024-08-20 13:06:05 +0530

    feat: Add Footer and NavBar components, update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Travel App",
@@ -14,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <NavBar/>
+      <main className="relative overflow-hidden">{children}</main>
+      <Footer/>
     </html>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import Link from "next/link"
+
 
 const NavBar = () => {
   return (
-    <div>
-      NavBar
-    </div>
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+        <Link href="/">
+
+        </Link>
+    </nav>
   )
 }
 
